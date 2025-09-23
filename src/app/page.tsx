48e2746233ade06d24105020fa1ffbfdc8fdf128
aditya45210l <aditya45210l@gmail.com>
2025-09-23 15:19:42 +0530

    remove incryption bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,9 @@ import ContactSection from "@/components/contact";
 import FeaturesSection from "@/components/features-8";
 import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
-import { decryptAPIKey, encryptAPIKey } from "@/lib/actions/EncryptDecript";
 
 const Home = () => {
-  const incryptedKey = encryptAPIKey(
-    "0x25BBe47DfDA88AA7BF153110736633d0c344B3e2"
-  );
-  console.log("incrypted key: ", incryptedKey);
-  const decryptedKey = decryptAPIKey(incryptedKey);
-  console.log("decrypted key: ", decryptedKey);
+ 
   return (
     <div className="">
       {/* <ModeToggle/>
